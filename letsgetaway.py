d0e41866b0664cbe908f3fce0e566ffdd5855c82
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2016-02-26 11:05:31 -0500

    removed destinations.txt and updates to print more info, basic tested

--- letsgetaway.py
+++ letsgetaway.py
@@ -1,9 +1,9 @@
 import requests, json
 from operator import itemgetter
 
-
 class Destination:
-	def __init__(self, cityName, countryName, shortName, kayakUrl, departDate, returnDate, airline, price):
+	def __init__(self, originCity, cityName, countryName, shortName, kayakUrl, departDate, returnDate, airline, price):
+		self.originCity = originCity
 		self.cityName = cityName
 		self.countryName = countryName
 		self.shortName = shortName
@@ -13,8 +13,30 @@ class Destination:
 		self.airline = airline
 		self.price = price
 	def toString(self):
-		return "%s, %s, %s, %s, %s, %s, %s" % (self.cityName, self.countryName, self.shortName, self.kayakUrl, self.departDate, self.returnDate, self.airline)
-		
+		return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.originCity, self.cityName, self.countryName, self.shortName, self.kayakUrl, self.departDate, self.returnDate, self.airline, self.price)
+
+def commonDestination(shortName, trips):
+	commonDest=True
+	for orig in trips:
+			if shortName not in [d.shortName for d in trips[orig].values()]:
+				commonDest=False
+	return commonDest
+
+def encode(text):
+    return text.encode('utf-8')
+   	
+def writeTripOptionsToFile(filename, trips, sortedShortNameList):
+	f = open(filename+'.txt', 'w')
+	for tup in sortedShortNameList:
+		shortName=tup[0]
+		price=tup[1]
+		f.write("%s:%s" % (shortName, price))
+		f.write("--Trips: ")
+		for orig in trips:
+			f.write(encode(trips[orig][shortName].toString()) + ", ")
+		f.write("\n")
+	f.close()
+
 windowStart='20160401'
 windowEnd='20160430'
 trips = {}
@@ -22,33 +44,21 @@ originCities = ['JFK','LAX']
 for orig in originCities:
 	r = requests.get("https://www.kayak.com/h/explore/api?airport=%s&depart=%s&return=%s" % (orig, windowStart, windowEnd),headers={'User-Agent': 'curl -A "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'})
 	j = json.loads(r.text)
-	tripOptions = []
+	tripOptions = {}
 	for dest in j['destinations']:
-		destObj = Destination(dest['city']['name'], dest['country']['name'], dest['airport']['shortName'], dest['flightInfo']['url'], dest['depart'], dest['return'], dest['airline'], dest['flightInfo']['price'])
-		tripOptions.append(destObj)
-		# tripOptions.sort(key=lambda el: el.price)
-		
-		# tripOptions[dest['airport']['shortName']] = dest['flightInfo']['price']
-	# tripOptions = sorted(tripOptions, key=itemgetter(2))
+		destObj = Destination(orig, dest['city']['name'], dest['country']['name'], dest['airport']['shortName'], dest['flightInfo']['url'], dest['depart'], dest['return'], dest['airline'], dest['flightInfo']['price'])
+		tripOptions[destObj.shortName] = destObj
 	trips[orig]=tripOptions
 
-keyCity = originCities[0]
 priceByDest={}
 for orig in originCities:
-	for dest, price in trips[orig].iteritems():
-		commonDest = True
-		for orig in originCities:
-			if dest not in trips[orig].keys():
-				commonDest = False
-		if not commonDest:
+	for shortName in trips[orig]:
+		if not commonDestination(shortName, trips):
 			continue
-		if dest in priceByDest:
-			priceByDest[dest]+=price
+		if shortName in priceByDest:
+			priceByDest[shortName]+=trips[orig][shortName].price
 		else:
-			priceByDest[dest]=price
+			priceByDest[shortName]=trips[orig][shortName].price
 sorted_priceByDest = sorted(priceByDest.items(), key=itemgetter(1))
 
-f = open('destinations.txt','w')
-for item in sorted_priceByDest:
-	f.write("%s %s\n" % (item[0], item[1]))
-f.close()
+writeTripOptionsToFile("destinations", trips, sorted_priceByDest)
